65e4fd2cfa7975d82d94553282961ccee951fdc9
jasonbutler42 <jasonbutler42@gmail.com>
2014-07-24 10:07:06 -0500

    modified config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
  # The project repo name. (one word: no spaces, dashes, or underscores)
-set :application, "test" #FIXME
+set :application, "jasonbutler42.com" #FIXME
 
 # Legacy from porting from drupal, best to just leave this as "default"
 set :domains, ["default"]
 
 # Set the repository type and location to deploy from.
 set :scm, :git
-set :repository,  "git@github.com:kronda/wp_capistrano.git" #FIXME
+set :repository,  "https://github.com/jasonbutler42/jasonbutler42.com.git" #FIXME
 # set :scm, :subversion
 # set :repository,  "https://svn.example.com/svn/#{application}/trunk/"
 # set(:scm_password) { Capistrano::CLI.password_prompt("SCM Password: ") }
@@ -17,12 +17,12 @@ set(:db_root_pass) { Capistrano::CLI.password_prompt("Production Root MySQL pass
 set(:db_pass) { random_password }
 
 # The subdirectory within the repo containing the DocumentRoot.
-set :app_root, "webroot"
-set :local_path, "/Path/to/your/local-folder" #FIXME
+set :app_root, "wordpress"
+set :local_path, "/workspace/jasonbutler42.com" #FIXME
 
 # Use a remote cache to speed things up
 set :deploy_via, :remote_cache
-ssh_options[:user] = 'username' #FIXME
+ssh_options[:user] = 'addtwops' #FIXME
 
 # Multistage support - see config/deploy/[STAGE].rb for specific configs
 set :default_stage, "dev"
@@ -39,8 +39,6 @@ set :httpd_group, 'apache'
 
 
 #set this to where you have installed your wp site locally (http://[local_domain]/wp-admin/ should exist. )
-<<<<<<< HEAD
-set :local_domain, 'localhost.com' #FIXME
-=======
-set :local_domain, 'local.domain.com'
->>>>>>> 4201a433b092ad8658bc1e117f1b2ba6fdc2c0ad
+
+set :local_domain, 'local.jasonbutler42.com' #FIXME
+
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,18 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
+set :deploy_to, "/home/addtwops/public_html/#{application}-#{stage}" 
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
 set :branch, "dev" #FIXME
 
 # The hostnames to deploy to.
-role :web, "yourdomain.com" #FIXME
+role :web, "jasonbutler42.com" #FIXME
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "yourdomain.com", :primary => true #FIXME
+role :db, "jasonbutler42.com", :primary => true #FIXME
 
 # The path to wp-cli
-set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
+set :wp, "cd #{current_path}/#{app_root} ; /usr/php/54/usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
 
 # The username on the target system, if different from your local username
 ssh_options[:user] = 'username' #FIXME
